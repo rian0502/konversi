7af1fafb5ed79937a8a47b0c7b20a3ed20fa8ec5
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2022-04-06 19:39:07 +0700

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Kalkulator Konvers',
+      title: 'Kalkulator Konversi',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.red,
